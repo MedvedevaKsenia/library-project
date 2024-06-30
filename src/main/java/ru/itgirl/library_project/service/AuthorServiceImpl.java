8c0fa8eb54cef4b135852ccfac79a0d41726c0c0
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-07-01 00:42:18 +0500

    feat: app created

---
+++ src/main/java/ru/itgirl/library_project/service/AuthorServiceImpl.java
@@ -0,0 +1,42 @@
+package ru.itgirl.library_project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.itgirl.library_project.dto.AuthorDto;
+import ru.itgirl.library_project.dto.BookDto;
+import ru.itgirl.library_project.model.Author;
+import ru.itgirl.library_project.repository.AuthorRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    @Override
+    public AuthorDto getAuthorById(Long id) {
+        Author author = authorRepository.findById(id).orElseThrow();
+        AuthorDto authorDto = convertEntityToDto(author);
+        return authorDto;
+    }
+
+    private AuthorDto convertEntityToDto(Author author) {
+        List<BookDto> bookDtoList = author.getBooks().stream()
+                .map(book -> BookDto.builder()
+                        .genre(book.getGenre().getName())
+                        .name(book.getName())
+                        .id(book.getId())
+                        .build())
+                .toList();
+
+        AuthorDto authorDto = AuthorDto.builder()
+                .id(author.getId())
+                .name(author.getName())
+                .surname(author.getSurname())
+                .books(bookDtoList)
+                .build();
+        return authorDto;
+    }
+}
