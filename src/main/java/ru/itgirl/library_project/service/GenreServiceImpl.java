aadbdf6d130f2e75638dd6af33fbddaa19d28920
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-07-01 01:06:12 +0500

    feat: methods created

---
+++ src/main/java/ru/itgirl/library_project/service/GenreServiceImpl.java
@@ -0,0 +1,43 @@
+package ru.itgirl.library_project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.itgirl.library_project.dto.AuthorDto;
+import ru.itgirl.library_project.dto.BookDto;
+import ru.itgirl.library_project.dto.GenreDto;
+import ru.itgirl.library_project.model.Genre;
+import ru.itgirl.library_project.repository.GenreRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GenreServiceImpl implements GenreService {
+
+    private final GenreRepository genreRepository;
+
+    @Override
+    public GenreDto getGenreById(Long id) {
+        Genre genre = genreRepository.findById(id).orElseThrow();
+        GenreDto genreDto = convertEntityGenreToDto(genre);
+        return genreDto;
+    }
+
+    private GenreDto convertEntityGenreToDto(Genre genre) {
+        List<BookDto> bookDtoList = genre.getBooks().stream()
+                .map(book -> BookDto.builder()
+                        .genre(book.getGenre().getName())
+                        .name(book.getName())
+                        .id(book.getId())
+                        .build())
+                .toList();
+
+        AuthorDto authorDto = AuthorDto.builder()
+                .id(author.getId())
+                .name(author.getName())
+                .surname(author.getSurname())
+                .books(bookDtoList)
+                .build();
+        return authorDto;
+    }
+}
