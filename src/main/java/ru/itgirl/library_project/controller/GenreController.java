689ebb1c04f65e69dd8c5c50d66f8f14347cd77e
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-07-01 16:17:35 +0500

    fix: methods fixed

--- src/main/java/ru/itgirl/library_project/controller/GenreController.java
+++ src/main/java/ru/itgirl/library_project/controller/GenreController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import ru.itgirl.library_project.dto.GenreDto;
+import ru.itgirl.library_project.service.GenreService;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/ru/itgirl/library_project/service/GenreServiceImpl.java
+++ src/main/java/ru/itgirl/library_project/service/GenreServiceImpl.java
@@ -2,9 +2,9 @@ package ru.itgirl.library_project.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.itgirl.library_project.dto.AuthorDto;
 import ru.itgirl.library_project.dto.BookDto;
 import ru.itgirl.library_project.dto.GenreDto;
+import ru.itgirl.library_project.model.Book;
 import ru.itgirl.library_project.model.Genre;
 import ru.itgirl.library_project.repository.GenreRepository;
 
@@ -19,25 +19,24 @@ public class GenreServiceImpl implements GenreService {
     @Override
     public GenreDto getGenreById(Long id) {
         Genre genre = genreRepository.findById(id).orElseThrow();
-        GenreDto genreDto = convertEntityGenreToDto(genre);
-        return genreDto;
+        return convertEntityGenreToDto(genre);
     }
 
     private GenreDto convertEntityGenreToDto(Genre genre) {
-        List<BookDto> bookDtoList = genre.getBooks().stream()
-                .map(book -> BookDto.builder()
-                        .genre(book.getGenre().getName())
-                        .name(book.getName())
-                        .id(book.getId())
-                        .build())
-                .toList();
+        //List<BookDto> bookDtoList = BookDto.builder
+            //    .map(book -> BookDto.
+             //           builder()
+             //           .id(book.getId())
+             //           .name(book.getName())
+             //   .authors(book.getAuthors())
+              //          .build()
+      //  ).toList();
 
-        AuthorDto authorDto = AuthorDto.builder()
-                .id(author.getId())
-                .name(author.getName())
-                .surname(author.getSurname())
-                .books(bookDtoList)
+        GenreDto genreDto = GenreDto.builder()
+                .id(genre.getId())
+                .name(genre.getName())
+               // .books(bookDtoList)
                 .build();
-        return authorDto;
+        return genreDto;
     }
 }
