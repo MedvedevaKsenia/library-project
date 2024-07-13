863c2afdfde29ec5d0302dc8d6e48fac083f2dc6
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-07-13 13:49:42 +0500

    feat: method findbookbynameV1 created

---
+++ src/main/java/ru/itgirl/library_project/controller/BookController.java
@@ -0,0 +1,19 @@
+package ru.itgirl.library_project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itgirl.library_project.dto.BookDto;
+import ru.itgirl.library_project.service.BookService;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping("/book")
+    BookDto getBookByNameV1(@RequestParam(value = "name", required = false) String name) {
+        return bookService.getByNameV1(name);
+    }
+}
--- src/main/java/ru/itgirl/library_project/repository/BookRepository.java
+++ src/main/java/ru/itgirl/library_project/repository/BookRepository.java
@@ -3,5 +3,8 @@ package ru.itgirl.library_project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itgirl.library_project.model.Book;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findBookByName(String name);
 }
