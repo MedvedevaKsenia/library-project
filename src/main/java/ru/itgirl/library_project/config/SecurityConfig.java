a4a8f06f410ed21bdb5e93cb69274ba5e943b851
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-08-04 21:57:31 +0500

    fix: errors fixed

--- src/main/java/ru/itgirl/library_project/config/SecurityConfig.java
+++ src/main/java/ru/itgirl/library_project/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,6 +20,8 @@ public class SecurityConfig {
                         authorize.requestMatchers("/book").hasRole("USER")
                                 .requestMatchers("/book/v2").hasRole("ADMIN")
                                 .requestMatchers("/books").hasRole("ADMIN")
+                                .requestMatchers("/users").hasRole("ADMIN")
+                                .requestMatchers("/authors").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .httpBasic();
@@ -28,19 +29,18 @@ public class SecurityConfig {
     }
 
     @Bean
-    UserDetailsService user() {
+    public UserDetailsService users() {
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         UserDetails user = users
                 .username("user")
                 .password("password")
                 .roles("USER")
                 .build();
-
         UserDetails admin = users
                 .username("admin")
                 .password("password")
                 .roles("USER", "ADMIN")
                 .build();
-    return new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
