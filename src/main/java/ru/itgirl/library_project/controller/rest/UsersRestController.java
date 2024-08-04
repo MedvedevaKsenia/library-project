d8daed8952cadc20eeff03f2da0a56da5c1f57de
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-08-04 19:43:17 +0500

    feat: usersRestController created

---
+++ src/main/java/ru/itgirl/library_project/controller/rest/UsersRestController.java
@@ -0,0 +1,34 @@
+package ru.itgirl.library_project.controller.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.itgirl.library_project.dto.UsersCreateDto;
+import ru.itgirl.library_project.dto.UsersDto;
+import ru.itgirl.library_project.dto.UsersUpdateDto;
+import ru.itgirl.library_project.service.UsersService;
+
+@RestController
+@RequiredArgsConstructor
+public class UsersRestController {
+    private final UsersService usersService;
+
+    @GetMapping("/users/{id}")
+    UsersDto getUsersById(@PathVariable("id") Long id) {
+        return usersService.getUsersById(id);
+    }
+
+    @PostMapping("/users/create")
+    UsersDto createUsers(@RequestBody UsersCreateDto usersCreateDto) {
+        return usersService.createUsers(usersCreateDto);
+    }
+
+    @PutMapping("/users/update")
+    UsersDto updateUsers(@RequestBody UsersUpdateDto usersUpdateDto) {
+        return usersService.updateUsers(usersUpdateDto);
+    }
+
+    @DeleteMapping("/users/delete/{id}")
+    void deleteUsers(@PathVariable("id") Long id) {
+        usersService.deleteUsers(id);
+    }
+}
