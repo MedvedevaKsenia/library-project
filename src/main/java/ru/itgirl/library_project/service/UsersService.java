758d7da31887c04c39a6947289b6458919e9a68c
Kira Kami <ksenia.medvedeva.1993@gmail.com>
2024-08-04 18:03:32 +0500

    feat: usersupdatedto created

--- src/main/java/ru/itgirl/library_project/service/UsersService.java
+++ src/main/java/ru/itgirl/library_project/service/UsersService.java
@@ -1,9 +1,12 @@
 package ru.itgirl.library_project.service;
 
+import ru.itgirl.library_project.dto.UsersCreateDto;
 import ru.itgirl.library_project.dto.UsersDto;
 
 public interface UsersService {
     UsersDto getUsersById(Long id);
 
-    UsersDto createUsers()
+    UsersDto createUsers(UsersCreateDto usersCreateDto);
+
+    UsersDto updateUsers(UsersCreateDto )
 }
